9d9a09b3eacb458efa3cafb73fb2242ec8f83689
schef <zvanstefan@gmail.com>
2023-05-11 11:29:36 +0200

    kdenfile refactored

--- src/kdenlive_file.py
+++ src/kdenlive_file.py
@@ -2,6 +2,20 @@
 
 import sys
 import os
+from datetime import datetime, timedelta
+
+class Timestamp:
+    def __init__(self, timestamp_string):
+        self.timedelta = self.get_timedelta_from_timestamp_string(timestamp_string)
+
+    @staticmethod
+    def get_timedelta_from_timestamp_string(timestamp_string):
+        t = datetime.strptime(timestamp_string, '%H:%M:%S.%f')
+        return timedelta(hours=t.hour, minutes=t.minute, seconds=t.second, microseconds=t.microsecond)
+
+    def get_timestamp_string(self):
+        return str(self.timedelta)
+
 
 class Position:
     def __init__(self, x, y):
@@ -20,23 +34,30 @@ class Size:
         return f"w[{self.w}], h[{self.h}]"
 
 class Keyframe:
-    def __init__(self, position, size):
+    def __init__(self, timestamp, position, size):
+        self.timestamp = timestamp
         self.position = position
         self.size = size
 
     @staticmethod
-    def get_keyframe_from_transition_string(s):
-        _, s = s.split("=")
-        splited = s.split(" ")
-        return Position(int(splited[0]), int(splited[1])), Size(int(splited[2]), int(splited[3]))
+    def get_keyframe_from_keyframe_string(keyframe_string):
+        timestamp_string, position_size_string = keyframe_string.split("=")
+        timestamp = Timestamp(timestamp_string)
+        position_size_list = position_size_string.split(" ")
+        position = Position(int(position_size_list[0]), int(position_size_list[1]))
+        size = Size(int(position_size_list[2]), int(position_size_list[3]))
+        return Keyframe(timestamp, position, size) 
 
     @staticmethod
-    def get_transition_string_from_position_size(position, size):
-        return " ".join([str(position.x), str(position.y), str(size.w), str(size.h)])
+    def get_keyframe_string_from_keyframe(keyframe):
+        timestamp_string = keyframe.timestamp.get_timestamp_string()
+        position_size_string = " ".join([str(keyframe.position.x), str(keyframe.position.y), str(keyframe.size.w), str(keyframe.size.h)])
+        return timestamp_string + "=" + position_size_string
 
-    @staticmethod
-    def get_transition_string_from_keyframe(keyframe):
-        return " ".join([str(keyframe.position.x), str(keyframe.position.y), str(keyframe.size.w), str(keyframe.size.h)])
+    def __repr__(self):
+        return Keyframe.get_keyframe_string_from_keyframe(self)
+
+
 
 class KdenliveFile:
     LSTRIP = '    <property name="transition.rect">'
@@ -63,7 +84,7 @@ class KdenliveFile:
             lines = f.read().splitlines()
         return lines
 
-    def _get_new_name(self):
+    def _get_path_with_edited_name(self):
         splited = self.path.split(".")
         return ".".join(splited[:-1]) + "_edited" + "." + splited[-1]
 
@@ -71,31 +92,16 @@ class KdenliveFile:
         return KdenliveFile._read_lines_from_file(self.path)
 
     def _set_lines(self, lines):
-        KdenliveFile._write_lines_to_file(lines, f"{self._get_new_name()}")
+        KdenliveFile._write_lines_to_file(lines, f"{self._get_path_with_edited_name()}")
 
-    def get_transition_rect_line(self):
+    def _get_transition_rect_line(self):
         lines = self._get_lines()
         for l in lines:
             if "transition.rect" in l:
                 return l
         return ""
 
-    def get_keyframes(self):
-        keyframes = []
-        transition_rect_line = self.get_transition_rect_line()
-        transition_rect_line = transition_rect_line.lstrip(self.LSTRIP)
-        transition_rect_line = transition_rect_line.rstrip(self.RSTRIP)
-        for transition_string in transition_rect_line.split(";"):
-            keyframes.append(Keyframe.get_keyframe_from_transition_string(transition_string))
-        return keyframes
-
-    def generate_transition_string(self, timestamp, keyframe):
-        return f"{timestamp}={Keyframe.get_transition_string_from_keyframe(keyframe)}"
-
-    def generate_transition_rect_line(self, transition_string):
-        return self.LSTRIP + ";".join(transition_string) + self.RSTRIP
-
-    def replace_transition_rect_line(self, transition_rect_line):
+    def _set_transition_rect_line(self, transition_rect_line):
         lines = self._get_lines()
         index = -1
         for e,l in enumerate(lines):
@@ -104,9 +110,22 @@ class KdenliveFile:
         lines[index] = transition_rect_line
         self._set_lines(lines)
 
-    def set_transition_positions(self, transition_positions):
-        transition_rect_line = self.generate_transition_rect_line(transition_positions)
-        self.replace_transition_rect_line(transition_rect_line)
+
+    def _generate_transition_rect_line_from_keyframes(self, keyframes):
+        return self.LSTRIP + ";".join([Keyframe.get_keyframe_string_from_keyframe(k) for k in keyframes]) + self.RSTRIP
+
+    def get_keyframes(self):
+        keyframes = []
+        transition_rect_line = self._get_transition_rect_line()
+        transition_rect_line = transition_rect_line.lstrip(self.LSTRIP)
+        transition_rect_line = transition_rect_line.rstrip(self.RSTRIP)
+        for keyframe_string in transition_rect_line.split(";"):
+            keyframes.append(Keyframe.get_keyframe_from_keyframe_string(keyframe_string))
+        return keyframes
+
+    def set_keyframes(self, keyframes):
+        transition_rect_line = self._generate_transition_rect_line_from_keyframes(keyframes)
+        self._set_transition_rect_line(transition_rect_line)
 
 
 if __name__ == "__main__":
